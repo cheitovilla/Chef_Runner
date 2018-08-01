2acdfd5000bfd166e54a5eae4db47a8ca33eb83d
Cheitovilla <cheitovilla@outlook.com>
2018-08-01 17:14:38 -0500

    Camera follow player ready

---
+++ Chef/Assets/Scripts/Camerafollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camerafollow : MonoBehaviour 
+{
+
+	private Transform target;
+	private Vector3 offSet;
+
+	// Use this for initialization
+	void Start () 
+	{
+		target =	GameObject.FindGameObjectWithTag ("Player").transform;
+		offSet = transform.position - target.position;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.position = target.position + offSet;
+	}
+}
